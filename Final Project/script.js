93e94dd5eb9920c3840987923e6e2b747969903a
Your Name <you@example.com>
2024-06-17 08:51:47 -1000

    final project

---
+++ Final Project/script.js
@@ -0,0 +1,5 @@
+// Add event listener to the "Start Exploring" button
+document.querySelector('button').addEventListener('click', () => {
+    console.log('Button clicked!');
+    // Add functionality to navigate to the games page
+});
