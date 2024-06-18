95fa233e5c4169f6e4aa5507cd5c0763725e52f8
christr0i <col22020@byui.edu>
2024-06-17 22:00:32 -1000

    hp and about for FP

--- FinalProject/script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to the "Start Exploring" button
-document.querySelector('button').addEventListener('click', () => {
-    console.log('Button clicked!');
-    // Add functionality to navigate to the games page
-});
