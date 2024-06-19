6452077499f50b6ff92a8141ec335db465db2704
christr0i <col22020@byui.edu>
2024-06-18 17:36:06 -1000

    act 18

--- Activity-18/scripts/script.js
+++ Activity-18/scripts/script.js
@@ -4,12 +4,13 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    var number = document.getElementById('number').value
+    var number = document.getElementById('number').value;
 
     var usernameValid = /^[a-zA-Z0-9]{5,}$/.test(username); // Username should be at least 5 characters long and contain only letters and numbers
     var emailValid = /^[^@]+@\w+(\.\w+)+\w$/.test(email); // Simple email pattern check
     var passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password); // Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters
-    var numberValid =   /^\+\d{1,2}\s?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}$/.test(number);
+    var numberValid = /^\+\d{1,2}\s\d{3}[-.\s]?\d{3}[-.\s]?\d{4}$/.test(number);
+
 
     document.getElementById('usernameFeedback').style.display = usernameValid ? 'none' : 'block';
     document.getElementById('emailFeedback').style.display = emailValid ? 'none' : 'block';
@@ -19,7 +20,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     document.getElementById('usernameFeedback').textContent = usernameValid ? '' : 'Username should be at least 5 characters long and contain only letters and numbers.';
     document.getElementById('emailFeedback').textContent = emailValid ? '' : 'Please enter a valid email address.';
     document.getElementById('passwordFeedback').textContent = passwordValid ? '' : 'Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters.';
-    document.getElementById('numberFeedback').textContent = numberValid ? '' : 'Phone Number should be ten digits';
+    document.getElementById('numberFeedback').textContent = numberValid ? '' : 'Please enter a valid phone number (e.g., +1 800-123-4567).';
 
     var formValid = usernameValid && emailValid && passwordValid && numberValid;
 
